c83f92ead03da71c8a5926843f1606b93b91a4a4
Jingen Lin <jingen.lin.jl@gmail.com>
2013-12-23 13:03:18 -0500

    fix : 2013-12-23 13:03:18 -0500 Monday

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -14,6 +14,7 @@ class DocumentsController < ApplicationController
         doc.gen_thumbnail
       end
     end
+    render :nothing => true
   end
 
   def create
--- app/models/document.rb
+++ app/models/document.rb
@@ -7,7 +7,7 @@ class Document
   mount_uploader :file, DocumentUploader 
   has_mongoid_attached_file :image,
                             :styles => { 
-                              :thumbnail => "200x200^"
+                              :thumbnail => ["200x200^", :jpg]
                             },
                             :default_style => :original,
                             :default_url => '/img/doc_default.png',
@@ -20,7 +20,8 @@ class Document
   after_save :upload_to_crocodoc
 
   def gen_thumbnail
-    self.set(:image => StringIO.new(Crocodoc::Download.thumbnail(self.croc_uuid, 200, 200)))
+    self.image = StringIO.new(Crocodoc::Download.thumbnail(self.croc_uuid, 200, 200))
+    self.save
   end
 
   protected
